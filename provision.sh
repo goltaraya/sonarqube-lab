5a1cb340be3b3b13cb2160f918d45a9999834740
Yago Alexandre <goltarayago@gmail.com>
2023-04-03 12:59:03 -0300

    Installing Java 17 in VM

--- provision.sh
+++ provision.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 useradd sonar
-sudo yum -y install yum-utils
-sudo yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin wget unzip java-11-openjdk-devel > /dev/null/ 2>&1
-sudo systemctl enable docker
-sudo systemctl start docker
+sudo yum install -y wget unzip curl
+
+wget https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_linux-x64_bin.tar.gz
+tar xvf openjdk-17.0.2_linux-x64_bin.tar.gz
+sudo mv jdk-17.0.2/ /opt/jdk-17/
+cat << EOT >> ~/.bashrc
+JAVA_HOME=/opt/jdk-17
+PATH=$PATH:$JAVA_HOME/bin 
+EOT
+source ~/.bashrc
+
 wget -O /tmp/sonarqube.zip https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.9.0.65466.zip 
 unzip /tmp/sonarqube.zip -d /opt/
 sudo rm /tmp/sonarqube.zip
 sudo mv /opt/sonarqube-9.9.0.65466 /opt/sonarqube
 chown -R sonar:sonar /opt/sonarqube
+
 touch /etc/systemd/system/sonar.service
 echo > /etc/systemd/system/sonar.service
 cat << EOT >> /etc/systemd/system/sonar.service
@@ -22,10 +27,13 @@ After=syslog.target network.target
 Type=forking
 ExecStart=/opt/sonarqube/bin/linux-86x-64/sonar.sh start
 ExecStop=/opt/sonarqube/bin/linux-86x-64/sonar.sh stop
-User=sonar
-Group=sonar
+User=root
+Group=root
 Restart=always
 [Install]
 WantedBy=multi-user.target
 EOT
-service sonar start
+
+sudo systemctl daemon-reload
+sudo systemctl start sonar
+sudo systemctl enable sonar
