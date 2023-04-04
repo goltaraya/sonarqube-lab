5ce5631228cf0bd86052cdbd3936b0c8ba65cc58
Yago Alexandre <goltarayago@gmail.com>
2023-04-04 11:17:18 -0300

    Installing NodeJS and Sonar Scanner

--- provision.sh
+++ provision.sh
@@ -1,10 +1,16 @@
 #/usr/bin/bash
 useradd sonar
-yum install wget unzip java-11-openjdk-devel -y 
+
+#Installing Java 11, wget, unzip
+yum install wget unzip java-11-openjdk-devel -y
+
+# Downloading Sonarqube and installing it
 wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.1.0.47736.zip
 unzip sonarqube-9.1.0.47736.zip -d /opt/
 mv /opt/sonarqube-9.1.0.47736 /opt/sonarqube
 chown -R sonar:sonar /opt/sonarqube
+
+# Creating Sonar Service
 touch /etc/systemd/system/sonar.service
 echo > /etc/systemd/system/sonar.service
 cat <<EOT >> /etc/systemd/system/sonar.service
@@ -22,4 +28,16 @@ Restart=always
 WantedBy=multi-user.target
 EOT
 
+# Starting Sonar Service
 service sonar start
+
+# Installing Sonar Scanner
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.8.0.2856-linux.zip
+unzip sonar-scanner-cli-4.8.0.2856-linux.zip -d /opt/
+mv /opt/sonar-scanner-4.8.0.2856-linux /opt/sonar-scanner
+chown -R sonar:sonar /opt/sonar-scanner
+echo 'export PATH=$PATH:/opt/sonar-scanner/bin' | sudo tee -a /etc/profile
+
+# Installing NodeJS
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+yum install -y nodejs
